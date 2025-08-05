f88e314d36fc09cafe45a208173043b27cdc6479
nervus-tech <nervus.tech@gmail.com>
2025-08-05 12:10:19 +0300

    Add Spring Cloud 2025.0.0 support and Eureka client configuration for Notification service

--- notification/src/main/java/com/nervus/notification/controller/NotificationController.java
+++ notification/src/main/java/com/nervus/notification/controller/NotificationController.java
@@ -1,5 +1,12 @@
 package com.nervus.notification.controller;
 
-public class NotificationController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class NotificationController {
+    @GetMapping("/test")
+    public String test() {
+        return "Notification Service is running";
+    }
 }
